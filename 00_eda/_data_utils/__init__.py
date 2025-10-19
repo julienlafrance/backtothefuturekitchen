b899221b415cb939db1cab18625109b9a98823cc
Yassine M <mernissiyassine@hotmail.fr>
2025-10-19 21:58:32 +0200

    new file:   00_eda/00_eda.egg-info/PKG-INFO 	new file:   00_eda/00_eda.egg-info/SOURCES.txt 	new file:   00_eda/00_eda.egg-info/dependency_links.txt 	new file:   00_eda/00_eda.egg-info/requires.txt 	new file:   00_eda/00_eda.egg-info/top_level.txt 	modified:   00_eda/01_long_term/01_rating_analysis.ipynb 	modified:   00_eda/01_long_term/01_recipe_analysis.ipynb 	new file:   00_eda/01_long_term/01_recipe_analysis_old.ipynb 	new file:   00_eda/_data_utils/__init__.py 	renamed:    00_eda/00_data_utils.ipynb -> 00_eda/_data_utils/_data_utils.ipynb 	new file:   00_eda/_data_utils/data_utils_common.py 	renamed:    00_eda/data_utils.py -> 00_eda/_data_utils/data_utils_recipes_ratings.py 	new file:   00_eda/_data_utils/data_utils_recipes_recipes.py 	modified:   00_eda/pyproject.toml

---
+++ 00_eda/_data_utils/__init__.py
@@ -0,0 +1,17 @@
+# =============================================================================
+# _data_utils/__init__.py
+# Point d’entrée du module (BACKWARD COMPATIBLE)
+# =============================================================================
+"""
+🎯 Module unifié data_utils
+Permet d'importer toutes les fonctions communes, recettes et interactions
+avec un simple :
+    from data_utils import *
+"""
+
+# _data_utils/__init__.py
+from .data_utils_common import *
+from .data_utils_recipes_ratings import *
+from .data_utils_recipes_recipes import *
+
+print("✅ _data_utils module chargé (common + ratings + recipes)")
