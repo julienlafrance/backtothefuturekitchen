80214174b5e6f68d7291e5cb5581219b58da7d19
Julien Lafrance <julien@lafrance.io>
2025-10-25 17:57:39 +0200

    fix: Corriger chemins imports cached_loaders
    Erreur: ModuleNotFoundError mangetamain_analytics.data.cached_loaders
    
    Les imports doivent être relatifs au répertoire courant:
    - Avant: from mangetamain_analytics.data.cached_loaders import ...
    - Après: from data.cached_loaders import ...
    
    Cohérent avec: from utils import chart_theme

--- 10_preprod/src/mangetamain_analytics/visualization/analyse_ratings.py
+++ 10_preprod/src/mangetamain_analytics/visualization/analyse_ratings.py
@@ -19,7 +19,7 @@ from utils import chart_theme
 from utils import colors
 
 # Import des utilitaires de chargement avec cache
-from mangetamain_analytics.data.cached_loaders import get_ratings_longterm as load_ratings_for_longterm_analysis
+from data.cached_loaders import get_ratings_longterm as load_ratings_for_longterm_analysis
 
 # Import direct sans cache (utilisé rarement)
 from mangetamain_data_utils.data_utils_ratings import load_clean_interactions
